8be6c4b50ac9bdcfd28d96054e7f1e1af3b7ed1a
Pieter Belmans <pieterbelmans@gmail.com>
2023-02-01 10:57:45 +0100

    Moving some code to separate files

---
+++ assets/js/references.js
@@ -0,0 +1,126 @@
+references = {
+  "severi": {
+    "author" : "Severi, Francesco",
+    "year" : 1915,
+    "title" : "Sulla classificazione delle curve algebriche e sul teorema d'esistenza di Riemann",
+    "journal" : "Rendiconti della R. Accad. Naz. Lincei",
+    "number" : 24,
+    "pages" : [877, 888],
+
+    // TODO list comprehension
+    "dimensions": [
+      [2, 0, -1],
+      [3, 0, -1],
+      [4, 0, -1],
+      [5, 0, -1],
+      [6, 0, -1],
+      [7, 0, -1],
+      [8, 0, -1],
+      [9, 0, -1],
+      [10, 0, -1], // I guess based on the Mumford--Harris MathSciNet review
+    ]
+  },
+  "MR910206": {
+    "author" : "Eisenbud, David and Harris, Joe",
+    "year" : 1987,
+    "title" : "The Kodaira dimension of the moduli space of curves of genus $\\geq 23$",
+    "journal" : "Invent. Math.",
+    "number" : 2,
+    "pages" : [359, 387],
+
+    // TODO list comprehension
+    "dimensions": [
+      [24, 0, 24*3 - 3],
+      [25, 0, 25*3 - 3],
+      [26, 0, 26*3 - 3],
+    ]
+  },
+  "MR1953519": {
+    "author" : "Logan, Adam",
+    "year" : 2003,
+    "title" : "The Kodaira dimension of moduli spaces of curves with marked points",
+    "journal" : "Amer. J. Math.",
+    "volume" : 125,
+    "number": 1,
+    "pages" : [105, 138],
+
+    "dimensions" : [
+      [2, 11, -1],
+      [3, 14, -1],
+      [4, 15, -1],
+      [5, 12, -1],
+      [6, 15, -1],
+      [7, 11, -1],
+      [8, 5, -1],
+      [9, 8, -1],
+      [10, 10, -1],
+      [4, 17, 3*4 - 3 + 17],
+      [4, 16, ">=1"],
+      [5, 16, 3*5 - 3 + 16],
+      [5, 15, ">=1"],
+      [6, 17, 3*6 - 3 + 17],
+      [6, 16, ">=1"],
+      [7, 15, 3*7 - 3 + 15],
+      [7, 14, ">=1"],
+      [8, 14, 3*8 - 3 + 14],
+      [9, 13, 3*9 - 3 + 13],
+      [10, 15, 3*10 - 3 + 15],
+      [10, 14, ">=1"],
+      [11, 12, 3*11 - 3 + 12],
+      [11, 11, ">=1"],
+      [12, 13, 3*12 - 3 + 13],
+      [13, 11, 3*13 - 3 + 11],
+      [14, 11, 3*14 - 3 + 11],
+      [15, 11, 3*15 - 3 + 11],
+      [15, 10, ">=1"],
+      [16, 12, 3*16 - 3 + 12],
+      [17, 9, 3*17 - 3 + 9],
+      [18, 11, 3*18 - 3 + 11],
+      [19, 8, 3*19 - 3 + 8],
+      [20, 7, 3*20 - 3 + 7],
+      [21, 6, 3*21 - 3 + 6],
+      [21, 5, ">=0"],
+      [22, 8, 3*22 - 3 + 8]
+    ]
+  },
+  "MR2352722" : {
+    "author" : "Casnati, Gianfranco and Fontanari, Claudio",
+    "title" : "On the rationality of moduli spaces of pointed curves",
+    "journal" : "J. Lond. Math. Soc. (2)",
+    "volume" : 75,
+    "year" : 2007,
+    "number" : 3,
+    "pages" : [582, 596],
+
+    // the relevance is really in the rationality vs. unirationality
+    // Kodaira dimensions are only improving in 1 spot: (g,n)=(2,12)
+    "dimensions" : [
+      [2, 12, -1],
+      [3, 14, -1],
+      [4, 15, -1],
+      [5, 12, -1],
+    ]
+  },
+  "MR4247637" : {
+    "author" : "Agostini, Daniele and Barros, Ignacio",
+    "title" : "Pencils on surfaces with normal crossings and the Kodaira dimension of $\\overline{\\mathcal{M}}_{g,n}$",
+    "journal" : "Forum Math. Sigma",
+    "volume" : 9,
+    "year" : 2021,
+
+    "dimensions" : [
+      [12, 7, -1],
+      [12, 11, 3*12 - 3 + 11],
+      [13, 4, -1],
+      [13, 11, 3*13 - 3 + 11],
+      [14, 3, -1],
+      [14, 10, 3*14 - 3 + 10],
+      [15, 2, -1],
+      [15, 10, 3*15 - 3 + 10],
+      [16, 0, "<=43"],
+      [16, 9, 3*16 - 3 + 9],
+    ],
+  }
+};
+
+
